265b0561407cfd0124c11999ad1a699531569221
Kenneth Cañega <kennethcanega@Kenneths-MacBook-Air.local>
2021-07-02 15:07:06 +0800

    Add route base url

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const adminRoutes = require('./admin.js');
 const shopRoutes = require('./shop.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>')
