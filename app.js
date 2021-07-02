253720d74c005d6b90bef2fdd49972ffbbce9b82
Kenneth Cañega <kennethcanega@Kenneths-MacBook-Air.local>
2021-07-02 14:37:24 +0800

    Use POST  & GET method

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
-    next(); // allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded());
+
+app.get('/add-product', (req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log("In another middleware!");
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/', (req, res, next) => {
     res.send("<h1>Hello from Express</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
