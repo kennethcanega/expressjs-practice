a3e3b6a7c824c9732005871f7c4e1102c2002db9
Kenneth Cañega <kennethcanega@Kenneths-MacBook-Air.local>
2021-07-02 15:01:35 +0800

    Implement routes

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require('./admin.js');
+const shopRoutes = require('./shop.js');
 
-app.get('/add-product', (req, res, next) => {
-    console.log("In another middleware!");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/', (req, res, next) => {
-    res.send("<h1>Hello from Express</h1>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
