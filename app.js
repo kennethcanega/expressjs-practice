d9d7b79548fd4dd75754afba5aa31743662bc4e7
Kenneth Cañega <kennethcanega@Kenneths-MacBook-Air.local>
2021-07-03 22:34:52 +0800

    Added Pug - templating engine

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path')
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('vews', 'views');
+
 const adminRoutes = require('./admin.js');
 const shopRoutes = require('./shop.js');
 
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
